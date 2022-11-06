79bfa4f91d89ab31de81b47ddbcde127346ab207
José Augusto <joseaugusto.mello01@gmail.com>
2022-11-06 12:43:47 -0300

    Criando operacao de instancicao

--- src/main/java/com/jin/estudomc/EstudomcApplication.java
+++ src/main/java/com/jin/estudomc/EstudomcApplication.java
@@ -1,13 +1,34 @@
 package com.jin.estudomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EstudomcApplication {
+import com.jin.estudomc.domain.Categoria;
+import com.jin.estudomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class EstudomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }
